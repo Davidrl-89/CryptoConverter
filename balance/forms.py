7efb9dd5bb8f81dbc799cf04e64046e160af24db
david <drobles988@gmail.com>
2022-08-07 14:04:01 +0200

    feat: termino de crear formulario

--- balance/forms.py
+++ balance/forms.py
@@ -1,20 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField ,HiddenField , SelectField, SubmitField
+from wtforms import  FloatField ,HiddenField , SelectField, SubmitField
+from wtforms.validators import DataRequired, NumberRange ,ValidationError
 
-'''
-ME FALTA PONER DATOS REQUERIDOS
-'''
+
+def validar_moneda(form, field):
+    if field.data == form.moneda_from.data:
+        raise ValidationError("Debe elegir distintos tipos de monedas")
+
+
 
 class movimientosForm(FlaskForm):
     id = HiddenField()
     fecha = HiddenField("Fecha")
-    moneda1 = SelectField("From: ", choices=[
-        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")])
-    moneda2 = SelectField("To: ", choices=[
-        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")])
-    cantidad = DecimalField("Cantidad", places=8)
+    hora = HiddenField("Hora")
+
+    moneda_from = SelectField("From: ", choices=[
+        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")], validators=[DataRequired()])
+    moneda_to = SelectField("To: ", choices=[
+        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")], validators=[DataRequired(), validar_moneda])
+
+    cantidad_from = FloatField("Cantidad",validators=[DataRequired(message="La cantidad debe de ser un número positivo y mayor que 0"),
+    NumberRange(min=0.00001, max=99999999)] ,places=8)
+
     consultar = SubmitField("Consultar")
-    cantidad2 = DecimalField("Cantidad2")
-    PU = DecimalField("Precio unitario: ")
+
+    cantidad_to = FloatField("Cantidad2")
+    PU = FloatField("Precio unitario: ")
+
     borrar = SubmitField("Borrar")
     aceptar = SubmitField("Guardar")
