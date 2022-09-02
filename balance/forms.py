7747566ce43e26d2f2c8fea3d3f0a5c4089f89ea
david <drobles988@gmail.com>
2022-09-02 13:06:45 +0200

    feat: funciona  validar_moneda en formulario

--- balance/forms.py
+++ balance/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import  DateField, FloatField ,HiddenField ,TimeField ,SelectField, SubmitField
-from wtforms.validators import DataRequired, NumberRange 
+from wtforms.validators import DataRequired, NumberRange , ValidationError
 
 
-
+def validar_moneda(form,field):
+    if field.data == form.moneda_from.data:
+        raise ValidationError("Debes elegir diferentes tipos de moneda")
 
 class movimientosForm(FlaskForm):
     id = HiddenField()
@@ -13,7 +15,7 @@ class movimientosForm(FlaskForm):
     moneda_from = SelectField("From: ", choices=[
         ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")], validators=[DataRequired()])
     moneda_to = SelectField("To: ", choices=[
-        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")], validators=[DataRequired()])
+        ("EUR", "EUR"), ("BTC", "BTC"), ("ETH", "ETH"), ("LUNA", "LUNA"), ("LINK", "LINK")], validators=[DataRequired(), validar_moneda])
 
     cantidad_from = FloatField("Q:  ",validators=[DataRequired(message="La cantidad debe de ser un número positivo y mayor que 0"),
     NumberRange(min=0.00001, max=99999999)])
